efdf8f3e8e5fd4c8937c841ae1a8a98d6e440c7b
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-10-27 16:22:28 +0900

    Home画面から詳細画面にいかないバグを修正

--- src/components/QuestionDetail.jsx
+++ src/components/QuestionDetail.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { db } from '../firebase';
 import { collection, deleteDoc, doc, getDoc, getDocs } from 'firebase/firestore';
 import AnswerQuestion from './AnswerQuestion';
@@ -12,13 +12,13 @@ const QuestionDetail = () => {
 
     useEffect(() => {
         const fetchQuestionAndAnswers = async () => {
-            const questionDocRef = doc(db, 'questions', questionId);  // 修正された部分
+            const questionDocRef = doc(db, 'questions', questionId);
             const questionDoc = await getDoc(questionDocRef);
             if (questionDoc.exists()) {
                 setQuestion(questionDoc.data());
             }
-    
-            const querySnapshot = await getDocs(collection(questionDocRef, 'answers'));  // 修正された部分
+
+            const querySnapshot = await getDocs(collection(questionDocRef, 'answers'));
             const answerArray = querySnapshot.docs.map(doc => ({
                 id: doc.id,
                 ...doc.data()
@@ -32,13 +32,17 @@ const QuestionDetail = () => {
         const questionDocRef = doc(db, 'questions', questionId);
         await deleteDoc(questionDocRef);
     }
-    
 
     return (
         <div>
             {question && (
                 <div>
-                    <h1>{question.text}</h1>
+                    <h1>{question.title}</h1>
+                    <p><strong>URL:</strong> {question.url}</p>
+                    <p><strong>Development URL:</strong> {question.devUrl}</p>
+                    <p><strong>Details:</strong> {question.details}</p>
+                    <p><strong>Required Skills:</strong> {question.skills}</p>
+
                     {question && user && question.author && (
                         question.author.uid === user.uid && (
                             <button onClick={deleteQuestion}>Delete Question</button>
@@ -53,7 +57,7 @@ const QuestionDetail = () => {
                 {answers.map(answer => (
                     <div key={answer.id}>
                         <img src={answer.userImage} alt={`${answer.username}'s profile`} />
-                        <p><strong>{answer.username}:</strong> 
+                        <p><strong>{answer.username}:</strong>
                         {answer.text}</p>
                     </div>
                 ))}
--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import DeleteQuestion from "./DeleteQuestion";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import AnswerQuestion from "./AnswerQuestion";
 
 const QuestionList = () => {
@@ -23,12 +23,15 @@ const QuestionList = () => {
     return (
         <div>
             {questions.map(question => (
-                <div key={question.id}>
-                    <img src={question.author.userImage} alt={`${question.author.username}'s profile`} />
-                    <Link to={`/questions/${question.id}`}>{question.text}</Link>
-                    
-                    <p>Asked by: {question.author ? question.author.displayName : 'Anonymous'}</p>
-                    <div className=" flex"> 
+                <div key={question.id} className="mb-4 p-4 border border-gray-300 rounded-lg">
+                    <div className="flex items-center mb-2">
+                        <img src={question.author.userImage} alt={`${question.author.displayName}'s profile`} className="w-12 h-12 rounded-full mr-4" />
+                        <div>
+                            <Link to={`/questions/${question.id}`} className="text-xl font-bold text-blue-500 hover:underline">{question.question}</Link>
+                            <p className="text-gray-700">Asked by: {question.author ? question.author.displayName : 'Anonymous'}</p>
+                        </div>
+                    </div>
+                    <div className="flex"> 
                         <AnswerQuestion questionId={question.id} />
                         <DeleteQuestion questionId={question.id} />
                     </div>
@@ -38,4 +41,4 @@ const QuestionList = () => {
     )
 }
 
-export default QuestionList
+export default QuestionList;
