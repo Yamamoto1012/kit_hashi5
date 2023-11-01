16e14944515d0740f8eb5a6dfa876149e1e98934
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-11-01 16:22:38 +0900

    AI産デザイン

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,39 +11,37 @@ const Navbar = () => {
   }
 
   return (
-    <nav className=''>
-      <div className='flex justify-between'>
+    <nav className='bg-gray-800 text-white shadow-md p-4'>
+      <div className='container mx-auto flex justify-between'>
         <div className=" text-4xl">
-          <Link to="/">Home</Link>
+          <Link className="hover:text-gray-400 transition duration-300" to="/">Home</Link>
         </div>
-        <div className=' flex'>
-            <div className=" text-2xl pr-2">
-              <Link to="member">Member</Link>
+        <div className=' flex space-x-4'>
+            <div className=" text-2xl">
+              <Link className="hover:text-gray-400 transition duration-300" to="member">Member</Link>
             </div>
-            <div className=" text-2xl pr-2">
-              <Link to="announcement">Announcement</Link>
+            <div className=" text-2xl">
+              <Link className="hover:text-gray-400 transition duration-300" to="announcement">Announcement</Link>
             </div>
-            <div className=" text-2xl pr-2">
-              <Link to="post">Post</Link>
+            <div className=" text-2xl">
+              <Link className="hover:text-gray-400 transition duration-300" to="post">Post</Link>
             </div>
-            <div className=" text-2xl pr-2">
-              <Link to="login">Login</Link>
+            <div className=" text-2xl">
+              <Link className="hover:text-gray-400 transition duration-300" to="login">Login</Link>
             </div>
-            <div className=" text-2xl pr-2">
-              <Link to="logout">Logout</Link>
+            <div className=" text-2xl">
+              <Link className="hover:text-gray-400 transition duration-300" to="logout">Logout</Link>
             </div>
             {auth.currentUser && (
             <img
               src={auth.currentUser.photoURL}
               alt={`${auth.currentUser.displayName}'s profile`}
               onClick={handleProfileClick}
-              className="profile-icon"
+              className="profile-icon rounded-full w-10 h-10 object-cover border-2 border-white cursor-pointer"
             />
           )}    
         </div>
-
       </div>
-
     </nav>
   )
 }
