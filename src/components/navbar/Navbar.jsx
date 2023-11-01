15f4d9c58d30154bc406f1aadb89a8ab19e2ea41
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-11-01 16:29:16 +0900

    マッチ箱風デザイン

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,25 +11,27 @@ const Navbar = () => {
   }
 
   return (
-    <nav className='bg-gray-800 text-white shadow-md p-4'>
-      <div className='container mx-auto flex justify-between'>
-        <div className=" text-4xl">
-          <Link className="hover:text-gray-400 transition duration-300" to="/">Home</Link>
+    <nav className='bg-[#F6931D] text-white shadow-md p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div className="text-4xl">
+          <Link className="hover:text-gray-400 transition duration-300" to="/">
+            <img src="logo.png" alt="Logo" className="h-10" />
+          </Link>
         </div>
-        <div className=' flex space-x-4'>
-            <div className=" text-2xl">
+        <div className='flex space-x-4 justify-center items-center'>
+            <div className="text-2xl">
               <Link className="hover:text-gray-400 transition duration-300" to="member">Member</Link>
             </div>
-            <div className=" text-2xl">
+            <div className="text-2xl">
               <Link className="hover:text-gray-400 transition duration-300" to="announcement">Announcement</Link>
             </div>
-            <div className=" text-2xl">
+            <div className="text-2xl">
               <Link className="hover:text-gray-400 transition duration-300" to="post">Post</Link>
             </div>
-            <div className=" text-2xl">
+            <div className="text-2xl">
               <Link className="hover:text-gray-400 transition duration-300" to="login">Login</Link>
             </div>
-            <div className=" text-2xl">
+            <div className="text-2xl">
               <Link className="hover:text-gray-400 transition duration-300" to="logout">Logout</Link>
             </div>
             {auth.currentUser && (
@@ -37,7 +39,7 @@ const Navbar = () => {
               src={auth.currentUser.photoURL}
               alt={`${auth.currentUser.displayName}'s profile`}
               onClick={handleProfileClick}
-              className="profile-icon rounded-full w-10 h-10 object-cover border-2 border-white cursor-pointer"
+              className="rounded-full w-10 h-10 object-cover border-2 border-white cursor-pointer"
             />
           )}    
         </div>
