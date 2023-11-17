40e494fd82652ae0c9d854ce2fa09e7b61d7550d
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-11-17 15:13:05 +0900

    メンバーページを見やすく

--- src/components/MembersPage.jsx
+++ src/components/MembersPage.jsx
@@ -20,15 +20,17 @@ const MembersPage = () => {
     }, []);
 
     return (
-        <div>
-            <h1 className= "text-white text-2xl">メンバーリスト</h1>
-            <div className="grid grid-cols-4 gap-4">
-                {members.map(member => (
-                    <Link to={`/users/${member.id}`} key={member.id} className="flex flex-col items-center">
-                        <img src={member.photoURL} alt={`${member.displayName}のプロフィール画像`} className="w-24 h-24 rounded-full" />
-                        <div className= "text-white">{member.displayName}</div>
-                    </Link>
+        <div className=' pt-4'>
+            <div className="max-w-6xl mx-auto p-4 bg-[#222831] rounded-lg shadow">
+            <h1 className="text-3xl font-bold text-center text-white mb-8">メンバーリスト</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {members.map(member => (
+                <Link to={`/users/${member.id}`} key={member.id} className="flex flex-col items-center p-4 bg-white rounded-lg shadow hover:bg-gray-50 transition duration-300 ease-in-out">
+                    <img src={member.photoURL} alt={`${member.displayName}のプロフィール画像`} className="w-24 h-24 rounded-full mb-3" />
+                    <div className="text-gray-800 font-medium">{member.displayName}</div>
+                </Link>
                 ))}
+                </div>
             </div>
         </div>
     );
