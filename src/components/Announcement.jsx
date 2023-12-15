4582ad98519dc8e3f60802c3d60157907760db0e
Nakamura Toshiki <toshiki12g28n@gmail.com>
2023-12-15 15:04:34 +0900

    一時的にアンケートを閉鎖

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,13 +1,15 @@
 import { auth } from "../firebase";
 
-const Announcement = () => {
+const Announcement = ({ unreadMessages }) => {
+  const currentUser = auth.currentUser;
+  
   return (
     <div className="bg-gray-100 p-4 rounded-md shadow-md">
       <div className="mb-4">
         <p className="text-lg font-bold mb-2 text-gray-700">運営からのお知らせ</p>
         <div className="border-t border-gray-300 pt-2">
           <a
-            href="https://forms.office.com/r/dWtKFQ509i"
+            href=""/*"https://forms.office.com/r/dWtKFQ509i"*/
             className="text-blue-600 hover:text-blue-800 hover:underline"
             target="_blank" rel="noopener noreferrer"
           >
@@ -15,13 +17,17 @@ const Announcement = () => {
           </a>
         </div>
       </div>
-      {auth.currentUser && (
+      {currentUser && (
         <div>
-        <p className="text-lg font-bold mb-2 text-gray-700">あなたへのお知らせ (個人通知)</p>
-        <div className="border-t border-gray-300 pt-2">
-          {/* ここに個人通知のコンテンツを追加 */}
+          <p className="text-lg font-bold mb-2 text-gray-700">あなたへのお知らせ (個人通知)</p>
+          <div className="border-t border-gray-300 pt-2">
+            {unreadMessages > 0 && (
+              <p>
+                未読メッセージが {unreadMessages} 件あります。
+              </p>
+            )}
+          </div>
         </div>
-      </div>
       )}
     </div>
   );
