ab4f5780a1266efed6f99b3dfecc950457ab33e0
Nakamura Toshiki <toshiki12g28n@gmail.com>
2023-12-22 15:17:34 +0900

    えらー

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ import SearchQuestionButton from './SearchQuestionButton'
 const Home = () => {
   return (
     <>
-      <div>
+      <SearchQuestionButton />  
+      <div classname="bg-white">
         現在Firebaseのエラーで大多数の機能にアクセスできない状況になっています
         <br />
         アンケートの回答をするのが非常に難しい状況ですがお願いいたします
       </div>
-      <SearchQuestionButton />  
       <br></br>
         <QuestionList />
     </>
