713ca6540c3b87d82450e024f18696c6eb243292
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-22 14:00:23 +0900

    自己主張激しめ

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,7 +1,26 @@
 export default function Popup() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-blue-600 text-white p-4 text-center">
-      <p>シビックテックについて知らない方は <a href="https://civictech-web-2023.vercel.app/" target="_blank" rel="noopener noreferrer" className="underline">CivicTechWebへ</a></p>
+    <div className="fixed bottom-0 left-0 right-0 p-4 text-center rainbow-bg">
+      <style>
+        {`
+          @keyframes rainbow-animation {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
+          }
+          .rainbow-bg {
+            background: linear-gradient(270deg, red, orange, yellow, green, blue, indigo, violet);
+            background-size: 400% 400%;
+            animation: rainbow-animation 8s ease infinite;
+          }
+          .rainbow-text {
+            animation: rainbow 2s infinite;
+          }
+        `}
+      </style>
+      <p>こっちも見てほしいです... 
+        <a href="https://civictech-web-2023.vercel.app/" target="_blank" rel="noopener noreferrer" className="underline rainbow-text">CivicTechWebへ</a>
+      </p>
     </div>
   );
 }
